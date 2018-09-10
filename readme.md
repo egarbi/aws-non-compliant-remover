8c6f6f015f2a65a8d8ddba4631f49d027eb14d1a
Enrique Garbi <enrique.garbi@cloudreach.com>
2018-09-10 18:18:57 +0100

    Initial commit with S3Bucket and S3Object being tested

---
+++ readme.md
@@ -0,0 +1,31 @@
+# An automated approach to nuke non-compliant resources in a sandbox account (Proof of Concept)
+
+## Description
+The idea here is use a python script to retrive the list of compliant resources from a AWS Config rule
+and use [aws-nuke](https://github.com/rebuy-de/aws-nuke) to remove everything that doesnt match 
+the resources got it.
+The script can be run from Jenkins or similiar tools and an schedule way (probably daily)
+and clean up automatically the non-compliant resources.
+
+## Usage
+1. Clone this repo
+```
+git clone https://github.com/egarbi/aws-non-compliant-remover
+```
+2. Dependencies will be managed by pipenv
+```
+pip install pipenv
+```
+3. Install python dependencies
+```
+pipenv install
+```
+4. Run the config generator, will be used as an input of aws-nuke
+```
+python3 ./generate_aws-nuke_config.py
+```
+5. Remove all resources except those stated as in compliant by an AWS Config rule
+Too see how to have aws-nuke running see the doc online
+```
+./aws-nuke -c config.yaml 
+```
---
+++ retrieve_non_compliant_resources.py
@@ -0,0 +1,43 @@
+import boto3
+import yaml
+
+
+def set_dependant_resources(resource, configs):
+    resource_type = resource["EvaluationResultIdentifier"]["EvaluationResultQualifier"]["ResourceType"].replace("::","").replace("AWS","")
+    resource_id = resource["EvaluationResultIdentifier"]["EvaluationResultQualifier"]["ResourceId"]
+
+    # If S3 Bucket is listed Object need to be exluded as a dependency
+    if resource_type == "S3Bucket":
+        configs["accounts"]["369932205291"]["filters"]["S3Object"].append({"property": "Bucket", "value": resource_id})
+        resource_id = "s3://{}".format(resource_id)
+
+    configs["accounts"]["369932205291"]["filters"][resource_type].append(resource_id)
+
+def get_compliant_resources():
+  session = boto3.Session()
+  aws_config = session.client('config')
+  response = aws_config.get_compliance_details_by_config_rule(
+      ConfigRuleName='tags-compliance',
+      ComplianceTypes=[
+          'NON_COMPLIANT',
+      ],
+  )
+  return response["EvaluationResults"]
+
+# Read the template file
+with open("config_template.yaml", 'r') as stream:
+    try:
+        configs = yaml.load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+resources = get_compliant_resources()
+for resource in resources:
+    set_dependant_resources (resource, configs)
+
+# Save the definitive config
+with open("config.yaml", "w") as config:
+    try:
+        yaml.dump(configs, config)
+    except:
+        print ("exception")
